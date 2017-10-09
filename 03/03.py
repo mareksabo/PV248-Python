b5a4bbfe3d667153ea6573aa068ccb51ee936b65
Marek Šabo <xsabo4@fi.muni.cz>
2017-10-09 09:53:35 +0200

    3 partially finished

--- 03/03.py
+++ 03/03.py
@@ -1,4 +1,5 @@
 import re  # regular expressions
+from collections import Counter
 import sqlite3
 
 
@@ -17,7 +18,7 @@ class DBItem:
         if self.id is None:
             self.do_store()
             self.cursor.execute("select last_insert_rowid()")
-            id = self.cursor.fetchone()[0]
+            self.id = self.cursor.fetchone()[0]
 
 
 # Example of a class which represents a single row of a single database table.
@@ -26,18 +27,44 @@ class DBItem:
 
 
 class Person(DBItem):
+
+
     def __init__(self, conn, string):
         super().__init__(conn)
         self.name = re.sub('\([0-9+-]+\)', '', string)
-        # TODO: years born/died
+
+        bornDieRegex = re.compile(r"(.*) \((.*)--(.*)\)")
+        years = bornDieRegex.match(string)
+
+        born = years.group(2)
+        died = years.group(3)
+        if born is not None: self.born = int(born)
+        if died is not None: self.died = int()
 
     def fetch_id(self):
         self.cursor.execute("SELECT id FROM person WHERE name = ?", (self.name,))
         self.id = self.cursor.fetchone()
 
     def do_store(self):
-        self.cursor.execute("INSERT INTO person (name) VALUES (?)", (self.name,))
+        self.cursor.execute("INSERT INTO person (name, born, died) VALUES (?)", (self.name, self.born, self.died))
 
 
 if __name__ == '__main__':
-    print("Hello")
+
+    conn = sqlite3.connect('scorelib.dat')
+
+    keyValueRegex = re.compile(r"(.*): (.*)")
+
+    for line in open('scorelib.txt', 'r', encoding='utf-8'):
+        matchComposersWithYear = keyValueRegex.match(line)
+        if matchComposersWithYear is None: continue
+
+        key = matchComposersWithYear.group(1)
+        value = matchComposersWithYear.group(2)
+
+        if key == 'Composer':
+
+            for composerWithYear in value.split(";"):
+                p = Person(conn, composerWithYear.strip())
+                p.store()
+    conn.commit()
